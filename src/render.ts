c05d87f38cc9375b6b061df2534a567a33dccd7d
Teror Fox <99103591+sysfox@users.noreply.github.com>
2023-01-29 06:40:43 +0000

    Fix Test

--- src/render.ts
+++ src/render.ts
@@ -203,9 +203,6 @@ export async function renderPage(page: PageObjectResponse, notion: Client) {
     }
   }
 
-
-  // save update time
-  frontMatter.UPDATE_TIME = (new Date()).toISOString()
   // save nearest expiry time
   if (nearest_expiry_time) frontMatter.EXPIRY_TIME = nearest_expiry_time
  
